34f6aba20bd215c19544399d4d926f51a0a02c18
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2022-07-13 00:55:07 +0800

    Fix

--- tests/test_create_synapse.py
+++ tests/test_create_synapse.py
@@ -5,5 +5,5 @@ from jax.numpy import isclose
 def test():
     key     = PRNGKey(0)
     synapse = create_synapse(key, shape=(1000,))
-    assert isclose(synapse.mean(), 1.5, rtol=0.1)
-    assert isclose(synapse.std(),  0.1, rtol=0.1)
+    assert isclose(synapse.mean(), 1.5, atol=0.01)
+    assert isclose(synapse.std(),  0.1, atol=0.01)
