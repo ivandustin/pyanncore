90fb0c56c47fb204929ecbd1e412c86721d26c14
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2022-07-13 23:32:55 +0800

    Use left and right functions

--- anncore/neurogenesis.py
+++ anncore/neurogenesis.py
@@ -1,7 +1,9 @@
 from .create_synapse import create_synapse
 from jax.numpy import concatenate
 from jax.random import split
+from .right import right
+from .left import left
 
 def neurogenesis(key, network):
     keys = split(key, len(network))
-    return [ concatenate([ synapse[:,:-1], create_synapse(key, shape=(synapse.shape[0],1)), synapse[:,-1:] ], axis=1) for key, synapse in zip(keys, network) ] + [ create_synapse(key, shape=(1,1)) ]
+    return [ concatenate([ left(synapse), create_synapse(key, shape=(synapse.shape[0],1)), right(synapse) ], axis=1) for key, synapse in zip(keys, network) ] + [ create_synapse(key, shape=(1,1)) ]
