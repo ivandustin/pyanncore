3db07ac9ef0171c9611e82cc2fe06391dd786fe1
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2022-07-13 23:24:41 +0800

    Add test

--- tests/test_neurogenesis.py
+++ tests/test_neurogenesis.py
@@ -11,13 +11,23 @@ def test_a():
     assert network[1].shape == (1,1)
 
 def test_b():
+    key     = PRNGKey(0)
+    network = create_network(key, 100)
+    network = neurogenesis(key, network)
+    network = neurogenesis(key, network)
+    assert len(network) == 3
+    assert network[0].shape == (100,3)
+    assert network[1].shape == (1,2)
+    assert network[2].shape == (1,1)
+
+def test_c():
     key = PRNGKey(0)
     key_a, key_b = split(key)
     network_a = create_network(key_a, 100)
     network_b = neurogenesis(key_b, network_a)
     assert array_equal(network_a[0][:,0], network_b[0][:,-1])
 
-def test_c():
+def test_d():
     key = PRNGKey(0)
     key_a, key_b = split(key)
     network   = create_network(key, 100)
