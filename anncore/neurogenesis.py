65453b6f14d38e916a55fde7d8a5468816a4c61f
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2022-07-13 19:36:23 +0800

    Fix neurogenesis

--- anncore/neurogenesis.py
+++ anncore/neurogenesis.py
@@ -3,12 +3,5 @@ from jax.numpy import concatenate
 from jax.random import split
 
 def neurogenesis(key, network):
-    key_a, key_b = split(key)
-    root  = network[0]
-    n     = root.shape[0]
-    a     = create_synapse(key_a, shape=(n,1))
-    b     = create_synapse(key_b, shape=(1,1))
-    left  = root[:,:-1]
-    right = root[:,-1:]
-    root  = concatenate([left, a, right], axis=1)
-    return [root] + network[1:] + [b]
+    keys = split(key, len(network))
+    return [ concatenate([ synapse[:,:-1], create_synapse(key, shape=(synapse.shape[0],1)), synapse[:,-1:] ], axis=1) for key, synapse in zip(keys, network) ] + [ create_synapse(key, shape=(1,1)) ]
